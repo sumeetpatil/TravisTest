e9f74c071d78da6dd69d7c1ca606e8efa122b413
Sumeet Patil <sumeet.patil@sap.com>
2020-03-09 15:28:08 +0100

    test

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker login -u "sumeetp1991" -p "Clueless@123" docker.io
+docker login -u "sumeetp1991" -p $DOCKER_PASS docker.io
 docker build -t travis_test .
 docker tag travis_test sumeetp1991/travis_test:version1
 docker push sumeetp1991/travis_test:version1 
