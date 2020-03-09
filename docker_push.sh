fd024a40bdeab2e49bcff5efe6abcf0e28e40f2e
Sumeet Patil <sumeet.patil@sap.com>
2020-03-09 15:37:48 +0100

    Add keys

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker login -u "sumeetp1991" -p $DOCKER_PASS docker.io
+docker login -u $DOCKER_USERID -p $DOCKER_PASS docker.io
 docker build -t travis_test .
 docker tag travis_test sumeetp1991/travis_test:version1
 docker push sumeetp1991/travis_test:version1 
