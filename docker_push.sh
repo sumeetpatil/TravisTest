87234db676157c9420e778c97efb316285529999
Sumeet Patil <sumeet.patil@sap.com>
2020-03-11 10:03:27 +0100

    test

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "TRAVIS_TAG" $TRAVIS_TAG
+echo "$TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(\..+)?$"
+
 docker login -u $DOCKER_USERID -p $DOCKER_PASS docker.io
 docker build -t travis_test .
 docker tag travis_test sumeetp1991/travis_test:version1
